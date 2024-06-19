e9fcbec55ea8cafb073f092e898978a5753f2533
rtr_dnd <skysoyn@gmail.com>
2024-06-19 15:45:05 +0900

    font settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
 import type {Metadata} from 'next';
-import {Inter} from 'next/font/google';
+import {Archivo, Noto_Sans_JP} from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({subsets: ['latin']});
+const archivo = Archivo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-archivo',
+});
+const noto_sans_jp = Noto_Sans_JP({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-noto-sans-jp',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${archivo.variable} ${noto_sans_jp.variable} font-sans`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <CubeIcon className="w-10 h-10 text-blue-500" />
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -39,8 +38,86 @@ export default function Home() {
           priority
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <CubeIcon className="mt-20 w-10 h-10 text-blue-500" />
+      <div className="py-10">
+        <h3>左揃え</h3>
+        <p>
+          木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。
+          東ざかいのCherry
+          Blossom沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、22里余にわたる長い谿谷の間に散在していた。
+          【道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。名高い桟も、蔦のかずらを頼みにしたような危い場処ではなくなって、徳川時代の末にはすでに渡ることのできる橋であった。】
+          新規に新規にとできた道はだんだん谷の下の方の位置へと降って来た。
+          道の狭いところには、木を伐って並べ、藤づるでからめ、それで街道の狭いのを補った。
+          <span className="font-bold">
+            長い間にこの木曾路に起こって来た変化は、いくらかずつでも嶮岨な山坂の多いところをEasier-to-walkにした。
+          </span>
+        </p>
+      </div>
+      <div className="py-10">
+        <h3>両端揃え</h3>
+        <p className="text-justify">
+          木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。
+          東ざかいのCherry
+          Blossom沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、22里余にわたる長い谿谷の間に散在していた。
+          【道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。名高い桟も、蔦のかずらを頼みにしたような危い場処ではなくなって、徳川時代の末にはすでに渡ることのできる橋であった。】
+          新規に新規にとできた道はだんだん谷の下の方の位置へと降って来た。
+          道の狭いところには、木を伐って並べ、藤づるでからめ、それで街道の狭いのを補った。
+          <span className="font-bold">
+            長い間にこの木曾路に起こって来た変化は、いくらかずつでも嶮岨な山坂の多いところをEasier-to-walkにした。
+          </span>
+        </p>
+      </div>
+      <div className="py-10">
+        <h3>Alignment: left</h3>
+        <p>
+          Kiso Road is entirely surrounded by mountains. In some places, it
+          follows a cliffside path, in others, it runs along the banks of the
+          Kiso River, which drops tens of meters below, and in some places, it
+          winds around the entrances to valleys along mountain ridges. A single
+          highway cuts through this dense forest region. From Sakurasawa in the
+          east to Jumokutoge in the west, the eleven post stations of Kiso are
+          scattered along this highway, spanning over twenty-two ri through the
+          long valley. The road's position has changed several times, with the
+          old paths gradually becoming buried deep in the mountains. The famous
+          hanging bridge is no longer a perilous crossing relying on vines; by
+          the end of the Tokugawa era, it had already become a bridge that could
+          be crossed. The new roads gradually descended to the lower parts of
+          the valleys. In narrow places, trees were cut and lined up, and the
+          gaps were filled with wisteria vines to compensate for the narrowness
+          of the highway.
+          <br />
+          <span className="font-bold">
+            Over time, the changes that occurred along Kiso Road made it
+            somewhat easier to traverse these steep mountain paths.
+          </span>
+        </p>
+      </div>
+      <div className="py-10">
+        <h3>Alignment: justify</h3>
+        <p className="text-justify">
+          Kiso Road is entirely surrounded by mountains. In some places, it
+          follows a cliffside path, in others, it runs along the banks of the
+          Kiso River, which drops tens of meters below, and in some places, it
+          winds around the entrances to valleys along mountain ridges. A single
+          highway cuts through this dense forest region. From Sakurasawa in the
+          east to Jumokutoge in the west, the eleven post stations of Kiso are
+          scattered along this highway, spanning over twenty-two ri through the
+          long valley. The road's position has changed several times, with the
+          old paths gradually becoming buried deep in the mountains. The famous
+          hanging bridge is no longer a perilous crossing relying on vines; by
+          the end of the Tokugawa era, it had already become a bridge that could
+          be crossed. The new roads gradually descended to the lower parts of
+          the valleys. In narrow places, trees were cut and lined up, and the
+          gaps were filled with wisteria vines to compensate for the narrowness
+          of the highway.
+          <br />
+          <span className="font-bold">
+            Over time, the changes that occurred along Kiso Road made it
+            somewhat easier to traverse these steep mountain paths.
+          </span>
+        </p>
+      </div>
+      <div className="my-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
